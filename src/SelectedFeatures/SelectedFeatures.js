0878d3d81e06303521ced568d17159ee2e1cf2c1
Alex Walters <alexnwalters@gmail.com>
2019-04-28 19:25:36 -0400

    seperated into several react components

---
+++ src/SelectedFeatures/SelectedFeatures.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+class SelectedFeatures extends React.Component {
+    render() {
+        return(
+
+        )
+    }
+}
+
+export default SelectedFeatures
---
+++ src/TechForm/TechForm.js
@@ -0,0 +1,40 @@
+import React, { Component } from 'react';
+import './TechForm.css';
+
+class TechForm extends Component {
+    render() {
+        //how can I breack this down again, getting an error with the key
+        const specs = Object.keys(this.props.features)
+            .map(key => {
+                const options = this.props.features[key].map((item, index) => {
+                const selectedClass = item.name === this.props.selected[key].name ? 'feature__selected' : '';
+                const featureClass = 'feature__option ' + selectedClass;
+                return <li key={index} className="feature__item">
+                    <div className={featureClass}
+                    
+                    onClick={e => this.props.updateFeature(key, item)}>
+                        { item.name }
+                        ({ new Intl.NumberFormat('en-US', { style: 'currency', currency: 'USD'})
+                        .format(item.cost) })
+                    </div>
+                </li>
+                });
+
+                return <div className="feature" key={key}>
+                <div className="feature__name">{key}</div>
+                <ul className="feature__list">
+                    { options }
+                </ul>
+                </div>
+            });      
+
+        return (
+            <section className="main__form">
+                <h3>TECH SPECS AND CUSTOMIZATIONS</h3>
+                { specs }
+            </section>
+        )
+    }
+}
+
+export default TechForm
